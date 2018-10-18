53805448dd24720ac277738dcb8c8bc7104dba28
JeongRakkyu <JeongRakkyu@users.noreply.github.com>
2018-10-18 10:28:45 +0900

    [0.2.0] Add modified ECMB [Editor] JeongRakkyu [Desc] Now, you can use console function.
    ///Input text -> Press Enter(or Return) key///
    
    /********************** CAUTION **********************/
    1. The script should be placed behind </body> tag.
    2. Other script should be placed behind The ECMB.
    (If you don't, ECMB CANNOT check error.)
    3. You shuold input the existent variable(or function, etc.)

--- error_checker_for_mobile_browser.js
+++ error_checker_for_mobile_browser.js
@@ -4,7 +4,11 @@ var console_structure = ['<style>',
 '#button_container {width:100%; background-color:rgb(255, 213, 220); overflow:hidden;}',
 '.console_button{float:left; border:none; outline:none; background-color:rgb(253, 147, 147); color:rgb(252, 245, 188); padding:6px 10px; font-size:3vw;}',
 '#console_Element {background-color:rgb(255, 241, 220); width:100%; height:88%; overflow:auto; display:none; font-size:3vw;}',
-'#console_Console{background-color:rgb(255, 241, 220); width:100%; height:88%; overflow:auto; display:block; font-size:3vw;}',
+'#console_Console{background-color:rgb(255, 241, 220); width:100%; height:58%; overflow:auto; display:block; font-size:3vw;}',
+'#console_Console p{margin:0;}',
+'#log_container{background-color:rgb(255, 241, 220); width:100%; height:30%; overflow:auto; display:block;}',
+'#console_input{border:none; float:left; display:block; width:100%; height:3vw; font-size:3vw;}',
+'#console_log{display:block; float:left; font-size:3vw;}',
 '</style>',
 '<div id="console_container">',
 '<div id="button_container">',
@@ -12,26 +16,29 @@ var console_structure = ['<style>',
 '</div>',
 '<div id="console_Element">html object</div>',
 '<div id="console_Console">Has no ‘Javascript’ error.</div>',
+'<div id="log_container"><input id="console_input" type="text" onkeypress="if(event.keyCode == 13) {console_log()}"><div id="console_log"></div></div>',
 '</div>']
 var create_div = document.createElement("div");
 create_div.innerHTML = console_structure.join('');
 document.body.appendChild(create_div);
 
 //check error
-window.onerror = function handleErr(msg, url, line, col) {
-    var error_message = '';
+var error_message = '';
+onerror = function handleErr(msg, url, line, col) {
     error_message += '<p>Error Message : <span style="color:red;">' + msg + '</span></p>';
     error_message += '<p>Location : <span style="color:red;">' + url + '</span></p>';
     error_message += '<p>Line : <span style="color:red;">' + line + '</span></p>';
-    error_message += '<p>Column : <span style="color:red;">' + col + '</span></p>';
+    error_message += '<p>Column : <span style="color:red;">' + col + '</span></p><br>';
     document.getElementById("console_Console").innerHTML = error_message;
     return true;
 }
 
 //button control
 var con_con = document.getElementById("console_Console");
+var con_input = document.getElementById("log_container");
 var con_ele = document.getElementById("console_Element");
 var con_sw = 1;
+var con_sw2 = 1;
 var ele_sw = 0;
 
 function console_Console() {
@@ -40,6 +47,8 @@ function console_Console() {
         ele_sw = 0;
         con_con.style.display = "block";
         con_sw = 1;
+        con_input.style.display = "block";
+        con_sw2 = 1;
     }
 }
 
@@ -47,7 +56,17 @@ function console_Element() {
     if (con_sw === 1) {
         con_con.style.display = "none";
         con_sw = 0;
+        con_input.style.display = "none";
+        con_sw2 = 0;
         con_ele.style.display = "block";
         ele_sw = 1;
     }
+}
+
+//log function
+function console_log(){
+    var log_value = document.getElementById("console_input").value;
+    var log_function = new Function('return ' + log_value)();
+    var print = document.getElementById("console_log");
+    print.innerText = log_function;
 }
